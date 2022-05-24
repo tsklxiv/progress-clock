ec4629913cf97df480877296d0e6fa2519212226
Dang Hoang Tuan (Wumi4) <danghoangtuan526@gmail.com>
2022-05-24 18:12:42 +0700

    Add a seconds progress bar for exprimenting

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,16 @@
 import Head from 'next/head'
+import {useEffect, useState} from 'react'
 
 export default function Home() {
+  const [time, setTime] = useState(new Date())
+
+  useEffect(() => {
+    const interval = setInterval(() => setTime(new Date()), 1000)
+    return () => {
+      clearInterval(interval)
+    }
+  })
+
   return (
     <div className="container">
       <Head>
@@ -15,8 +25,11 @@ export default function Home() {
 
         <p className="description">
           The time as progress bars.
-          Influenced by <a href="https://neal.fun/progress/">neal.fun/progress</a>
+          Influenced by <a href="https://neal.fun/progress/">neal.fun/progress</a>.
         </p>
+
+        <progress className="second" value={time.getSeconds()} max="60"/>
+        <span className="second-left">{time.getSeconds()} seconds left</span>
       </main>
     </div>
   )
